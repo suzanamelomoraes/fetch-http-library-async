8bb2f96f8b961e7a086a49457d1b0aefeeb46fd7
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-30 12:49:29 +1300

    turn http request to post a new user into async function

--- asynchttplibrary.js
+++ asynchttplibrary.js
@@ -13,18 +13,15 @@ class EasyHTTP {
 
   // Make an HTTP POST request
   async post(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
     });
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP PUT request
