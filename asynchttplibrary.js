af9b4f77e0f78592c471ae2d45f7482c20b2904a
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-30 12:52:38 +1300

    turn http request to update a  user into async function

--- asynchttplibrary.js
+++ asynchttplibrary.js
@@ -25,19 +25,16 @@ class EasyHTTP {
   }
 
   // Make an HTTP PUT request
-  put(url, data) {
-    return new Promise((resolve, reject) => {
-      fetch(url, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json"
-        },
-        body: JSON.stringify(data)
-      })
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(data)
     });
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP DELETE request
