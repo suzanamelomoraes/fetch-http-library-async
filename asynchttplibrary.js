6ace31233dfae1a7fb095ea459c8775c89e3c141
Suzana-Melo-Moraes <suzanamelojor@gmail.com>
2020-01-30 12:47:03 +1300

    turn http request to get users into async function

--- asynchttplibrary.js
+++ asynchttplibrary.js
@@ -5,17 +5,14 @@
 
 class EasyHTTP {
   // Make an HTTP GET request
-  get(url) {
-    return new Promise((resolve, reject) => {
-      fetch(url)
-        .then(res => res.json())
-        .then(data => resolve(data))
-        .catch(err => reject(err));
-    });
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
   }
 
   // Make an HTTP POST request
-  post(url, data) {
+  async post(url, data) {
     return new Promise((resolve, reject) => {
       fetch(url, {
         method: "POST",
